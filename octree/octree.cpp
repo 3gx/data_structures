1f73f709fe34ba81658669a786669d261e597bd9
egaburov <egaburov@dds.nl>
2012-05-25 12:53:03 +0200

    added octree

---
+++ octree/octree.cpp
@@ -0,0 +1,104 @@
+#include <cstdio>
+#include <cstdlib>
+#include "plummer.h"
+#include "octree.h"
+#include "mytimer.h"
+
+int main(int argc, char * argv[])
+{
+  int n_bodies = 10240;
+  if (argc > 1) n_bodies = atoi(argv[1]);
+  assert(n_bodies > 0);
+  fprintf(stderr, "n_bodies= %d\n", n_bodies);
+
+  const double t00 = get_wtime();
+  Particle::Vector ptcl;
+  ptcl.reserve(n_bodies);
+#if 0
+  const Plummer data(n_bodies);
+  for (int i = 0; i < n_bodies; i++)
+  {
+    ptcl.push_back(Particle(data.pos[i], data.mass[i]));
+  }
+#else
+  for (int i = 0; i < n_bodies; i++)
+  {
+    ptcl.push_back(Particle(
+          vec3(drand48(), drand48(), drand48()),
+          1.0/n_bodies));
+  }
+#endif
+  
+  Octree::Body::Vector octBodies;
+  octBodies.reserve(n_bodies);
+
+  const double t10 = get_wtime();
+  fprintf(stderr, " -- Compute bounding box -- \n");
+
+  vec3 rmin(+HUGE);
+  vec3 rmax(-HUGE);
+
+  for (int i = 0; i < n_bodies; i++)
+  {
+    octBodies.push_back(Octree::Body(ptcl[i], i));
+    rmin = mineach(rmin, ptcl[i].pos);
+    rmax = maxeach(rmax, ptcl[i].pos);
+  }
+  const vec3 centre = (rmax + rmin)*0.5;
+  const vec3 vsize  =  rmax - rmin;
+  const real  size  = __max(__max(vsize.x, vsize.y), vsize.z);
+  real size2 = 1.0;
+  while (size2 > size) size2 *= 0.5;
+  while (size2 < size) size2 *= 2.0;
+
+  const int n_nodes = n_bodies;
+  Octree tree(centre, size2, n_nodes);
+
+  const double t20 = get_wtime();
+  fprintf(stderr, " -- Buidling octTree -- \n");
+  for (int i = 0; i < n_bodies; i++)
+  {
+    tree.push(octBodies[i], octBodies);
+  }
+  fprintf(stderr, "ncell= %d n_nodes= %d  depth= %d\n",
+      tree.ncell, tree.n_nodes, tree.depth);
+  assert(tree.sanity_check() == n_bodies);
+  const double t30 = get_wtime();
+  
+  fprintf(stderr, " -- Dump morton -- \n");
+  std::vector<int> morton_list;
+  morton_list.reserve(n_bodies);
+  tree.morton_dump(morton_list);
+  fprintf(stderr, "morton_list.size()= %d  n_bodies= %d\n",
+      (int)morton_list.size(), n_bodies);
+  for (std::vector<int>::iterator it = morton_list.begin(); it != morton_list.end(); it++)
+    assert(*it < n_bodies);
+  assert((int)morton_list.size() == n_bodies);
+  const double t40 = get_wtime();
+  
+  fprintf(stderr, " -- Shuffle octBodies -- \n");
+  Octree::Body::Vector octBodiesSorted;
+  octBodiesSorted.reserve(n_bodies);
+  for (std::vector<int>::iterator it = morton_list.begin(); it != morton_list.end(); it++)
+  {
+    assert(*it < n_bodies);
+    octBodiesSorted.push_back(octBodies[*it]);
+#if 1
+    fprintf(stdout, "%g %g %g \n", 
+        octBodiesSorted.back().pos.x,
+        octBodiesSorted.back().pos.y,
+        octBodiesSorted.back().pos.z);
+#endif
+  }
+  const double t50 = get_wtime();
+
+  fprintf(stderr, " Timing info: \n");
+  fprintf(stderr, " -------------\n");
+  fprintf(stderr, "   Plummer:  %g sec \n", t10 -t00);
+  fprintf(stderr, "   BBox:     %g sec \n", t20 -t10);
+  fprintf(stderr, "   Tree:     %g sec \n", t30 -t20);
+  fprintf(stderr, "   Morton:   %g sec \n", t40 -t30);
+  fprintf(stderr, "   Shuffle:  %g sec \n", t50 -t40);
+
+  return 0;
+}
---
+++ octree/octree.h
@@ -0,0 +1,191 @@
+#ifndef __OCTREE_H__
+#define __OCTREE_H__
+
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#include <list>
+#include "vector3.h"
+
+typedef float real;
+typedef vector3<real> vec3;
+
+template<class T>
+static inline T __min(const T &a, const T &b) {return a < b ? a : b;}
+template<class T>
+static inline T __max(const T &a, const T &b) {return a > b ? a : b;}
+
+
+#define SQR(x) ((x)*(x))
+
+struct Particle
+{
+  typedef std::vector<Particle> Vector;
+  vec3 pos;
+  int  id;
+
+  Particle(const vec3 &_pos, const int _id) :
+    pos(_pos), id(_id) {}
+};
+
+
+
+struct Octree
+{
+  enum {NLEAF = 16};
+  enum {EMPTY = -1};
+  enum {BODYX = -2};
+
+  struct Body
+  {
+    typedef std::vector<Body> Vector;
+    vec3 pos;
+    int  idx;
+
+    Body(const vec3 &_pos, const int _idx) : pos(_pos), idx(_idx) {}
+    Body(const Particle &p, const int _idx) : pos(p.pos), idx(_idx) {}
+  };
+
+  vec3 root_centre;
+  real root_size;
+  int depth;
+  int ncell;
+  int n_nodes;
+  std::vector<int> node_list;
+
+  Octree(const vec3 &_centre, const real _size, const int _n_nodes) :
+    root_centre(_centre), root_size(_size), depth(0), ncell(0), n_nodes(_n_nodes)
+  {
+    node_list.resize(n_nodes<<3);
+    for (std::vector<int>::iterator it = node_list.begin(); it != node_list.end(); it++)
+      *it = -1;
+  }
+
+  inline int reverse_int(const int a) {return BODYX-a;}
+
+  static inline int Octant(const vec3 &lhs, const vec3 &rhs) 
+  {
+    return
+      (((lhs.x <= rhs.x) ? 1 : 0) +  
+       ((lhs.y <= rhs.y) ? 2 : 0) + 
+       ((lhs.z <= rhs.z) ? 4 : 0));
+  }
+
+  static inline vec3 compute_centre(const vec3 &c, const real s, const int oct)
+  {
+    return vec3(
+        c.x + s * ((oct&1) ? (real)1.0 : (real)-1.0),
+        c.y + s * ((oct&2) ? (real)1.0 : (real)-1.0),
+        c.z + s * ((oct&4) ? (real)1.0 : (real)-1.0)
+        );
+  }
+
+  void check_overflow(const int n) const
+  {
+#if 1
+    assert(n < (int)node_list.size());
+#endif
+  }
+
+  int sanity_check() const
+  {
+    int nbody = 0;
+    int nnode = 0;
+    int empty = 0;
+    for (int i = 0; i < (const int)node_list.size(); i++)
+      if (node_list[i] == EMPTY)
+        empty++;
+      else if (node_list[i] >= 0)
+        nnode++;
+      else
+        nbody++;
+    fprintf(stderr, "empty= %d nnode= %d nbody= %d\n",
+        empty, nnode, nbody);
+    return nbody;
+  }
+
+
+  void push(const Body &body, const Body::Vector &bodies)
+  {
+    int child_idx = 0;   /* child idx inside a node */
+    int child     = 0;   /* child */
+    int locked    = 0;   /* locked cell */
+    int depth     = 0;   /* depth */ 
+
+    vec3 centre = root_centre;
+    real hsize  = root_size*(real)0.5;
+    while (child > EMPTY)  /* if positive, this means node is already there */
+    {
+      const int node = child;
+      depth++;
+
+      child_idx  = Octant(centre, body.pos);
+      hsize     *= (real)0.5;
+      centre     = compute_centre(centre, hsize, child_idx);
+
+      locked = node + child_idx;
+      check_overflow(locked);
+      child  = node_list[locked];
+//      fprintf(stderr, " depth= %d child= %d lock= %d\n", depth, child, locked);
+    }
+//    fprintf(stderr, " -- depth= %d child= %d lock= %d\n", depth, child, locked);
+
+    if (child == EMPTY)
+    {
+      node_list[locked] = reverse_int(body.idx);
+//      fprintf(stderr, " -- insert \n");
+    }
+    else
+    {
+      while((child = node_list[locked]) != EMPTY)
+      {
+        assert(child < EMPTY);
+        depth++;
+        ncell++;
+
+        const int cfirst = ncell<<3;
+        check_overflow(cfirst+7);
+        for (int k = 0; k < 8; k++)
+          node_list[cfirst + k] = EMPTY;
+        node_list[locked] = cfirst;
+
+        const int body_id = reverse_int(child);
+        assert(body_id >= 0);
+        child_idx = Octant(centre, bodies[body_id].pos);
+        node_list[cfirst + child_idx] = child;
+
+        child_idx = Octant(centre, body.pos);
+        hsize    *= (real)0.5;
+        centre    = compute_centre(centre, hsize, child_idx);
+        locked    = cfirst + child_idx;
+      }
+      node_list[locked] = reverse_int(body.idx);
+//      fprintf(stderr, " -- split: lock= %d\n", locked);
+    }
+
+    this->depth = __max(this->depth, depth);
+  }
+
+  void morton_dump(std::vector<int> &list)
+  {
+    for (int k = 0; k < 8; k++)
+      morton_dump_recursive(k, list);
+  }
+
+    void morton_dump_recursive(int node, std::vector<int> &list)
+    {
+      assert(node < (int)node_list.size());
+      const int cell = node_list[node];
+      if (cell == EMPTY) return;
+      if (cell >  EMPTY)
+      {
+        for (int k = 0; k < 8; k++)
+          morton_dump_recursive(cell+k, list);
+      }
+      else
+        list.push_back(reverse_int(cell));
+    }
+};
+
+
+#endif /* __OCTREE_H__ */
