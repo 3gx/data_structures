dce7af081ae0edfdd48646edf433f5b5af7b5e94
egaburov <egaburov@dds.nl>
2012-05-25 14:29:16 +0200

    added SSE version!

--- octree/octree.h
+++ octree/octree.h
@@ -18,6 +18,12 @@ static inline T __max(const T &a, const T &b) {return a > b ? a : b;}
 
 #define SQR(x) ((x)*(x))
 
+
+#ifdef __mySSE__
+typedef int    v4si  __attribute__((vector_size(16)));
+typedef float  v4sf  __attribute__((vector_size(16)));
+#endif
+
 struct Particle
 {
   typedef std::vector<Particle> Vector;
@@ -39,11 +45,33 @@ struct Octree
   struct Body
   {
     typedef std::vector<Body> Vector;
-    vec3 pos;
-    int  idx;
+    private:
+#ifdef __mySSE__
+    v4sf packed_pos;
+#else
+    vec3 _pos;
+    int  _idx;
+#endif
 
-    Body(const vec3 &_pos, const int _idx) : pos(_pos), idx(_idx) {}
-    Body(const Particle &p, const int _idx) : pos(p.pos), idx(_idx) {}
+    public:
+#ifdef __mySSE__
+    Body(const vec3 &pos, const int idx) 
+    {
+      packed_pos = (v4sf){pos.x, pos.y, pos.z, (float)idx};
+    }
+    Body(const Particle &p, const int idx) 
+    {
+      packed_pos = (v4sf){p.pos.x, p.pos.y, p.pos.z, (float)idx};
+    }
+    int idx() const {return (int)__builtin_ia32_vec_ext_v4sf(packed_pos, 3);}
+    v4sf pos() const {return packed_pos;}
+#else
+    Body(const vec3 &__pos, const int __idx) : _pos(__pos), _idx(__idx) {}
+    Body(const Particle &p, const int __idx) : _pos(p.pos), _idx(__idx) {}
+    int idx() const {return _idx;}
+    const vec3& pos() const {return _pos;}
+#endif
+
   };
 
   vec3 root_centre;
@@ -70,6 +98,14 @@ struct Octree
 
   static inline int reverse_int(const int a) {return BODYX-a;}
 
+#ifdef __mySSE__
+  static inline int Octant(const v4sf lhs, const v4sf rhs) 
+  {
+    int mask = __builtin_ia32_movmskps(
+        __builtin_ia32_cmpgeps(rhs, lhs));
+    return 7 & mask;
+  }
+#else
   static inline int Octant(const vec3 &lhs, const vec3 &rhs) 
   {
     return
@@ -77,15 +113,35 @@ struct Octree
        ((lhs.y <= rhs.y) ? 2 : 0) + 
        ((lhs.z <= rhs.z) ? 4 : 0));
   }
+#endif
 
-  static inline vec3 compute_centre(const vec3 &c, const real s, const int oct)
+#ifdef __mySSE__
+  static inline v4sf compute_centre(const v4sf centre, const int oct)
   {
+    static const v4sf off[8] = {
+      {-0.25f, -0.25f, -0.25f, -0.5f},
+      {+0.25f, -0.25f, -0.25f, -0.5f},
+      {-0.25f, +0.25f, -0.25f, -0.5f},
+      {+0.25f, +0.25f, -0.25f, -0.5f},
+      {-0.25f, -0.25f, +0.25f, -0.5f},
+      {+0.25f, -0.25f, +0.25f, -0.5f},
+      {-0.25f, +0.25f, +0.25f, -0.5f},
+      {+0.25f, +0.25f, +0.25f, -0.5f},
+    };
+    const v4sf len = __builtin_ia32_shufps(centre, centre, 0xff);
+    return centre + len * off[oct];
+  }
+#else
+  static inline vec3 compute_centre(const vec3 &c, real &s, const int oct)
+  {
+    s *= (real)0.5;
     return vec3(
         c.x + s * ((oct&1) ? (real)1.0 : (real)-1.0),
         c.y + s * ((oct&2) ? (real)1.0 : (real)-1.0),
         c.z + s * ((oct&4) ? (real)1.0 : (real)-1.0)
         );
   }
+#endif
 
   void check_overflow(const int n) const
   {
@@ -119,16 +175,23 @@ struct Octree
     int locked    = 0;   /* locked cell */
     int depth     = 0;   /* depth */ 
 
+#ifdef __mySSE__
+    v4sf     centre = {root_centre.x, root_centre.y, root_centre.z, root_size};
+#else
     vec3 centre = root_centre;
     real hsize  = root_size*(real)0.5;
+#endif
     while (child > EMPTY)  /* if positive, this means node is already there */
     {
       const int node = child;
       depth++;
 
-      child_idx  = Octant(centre, body.pos);
-      hsize     *= (real)0.5;
+      child_idx  = Octant(centre, body.pos());
+#ifdef __mySSE__
+      centre     = compute_centre(centre, child_idx);
+#else
       centre     = compute_centre(centre, hsize, child_idx);
+#endif
 
       locked = node + child_idx;
       check_overflow(locked);
@@ -153,12 +216,15 @@ struct Octree
 
         const int body_id = reverse_int(child);
         assert(body_id >= 0);
-        child_idx = Octant(centre, bodies[body_id].pos);
+        child_idx = Octant(centre, bodies[body_id].pos());
         node_list[cfirst + child_idx] = child;
 
-        child_idx = Octant(centre, body.pos);
-        hsize    *= (real)0.5;
+        child_idx = Octant(centre, body.pos());
+#ifdef __mySSE__
+        centre    = compute_centre(centre, child_idx);
+#else
         centre    = compute_centre(centre, hsize, child_idx);
+#endif
         locked    = cfirst + child_idx;
       }
       node_list[locked] = reverse_int(idx);
