d0b2e016253f1375fbe5c58d3a10ca2968281166
egaburov <egaburov@dds.nl>
2012-06-12 10:43:03 +0200

    added tanemura

---
+++ vorocell/tanemura/orig/genrand.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstdlib>
+
+int main()
+{
+  int n = 10000;
+  for (int i = 0; i < n; i++)
+  {
+    fprintf(stdout, " %d %g %g %g \n",
+        i+1,
+        drand48(),
+        drand48(),
+        drand48());
+  }
+  return 0;
+}
