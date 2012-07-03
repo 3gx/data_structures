e3711b3666ea786774176261d3459c8ac8149683
egaburov <egaburov.work@gmail.com>
2012-07-03 16:11:19 +0200

    found an algorithmic problem with direct polyherdron. have work around, but .... need a better algorithm

--- vorodia/DirectPoly.h
+++ vorodia/DirectPoly.h
@@ -16,7 +16,7 @@ struct DirectPolyhedron
       vec3 p;
       real h;
       HalfSpace() {}
-      HalfSpace(const vec3 &pos) : p(pos), h(pos*pos)
+      HalfSpace(const vec3 &pos) : p(pos), h(p*p)
       {
         assert(sizeof(HalfSpace) == 4*sizeof(real));
       }
@@ -37,29 +37,39 @@ struct DirectPolyhedron
   int  nface() const { return n; }
   int operator[](const int i) const { return nb[i]; }
 
-  bool push(const vec3 &pos, const int idx)
+  bool push(const vec3 &pos, const int idx, const real f = 1.0)
   {
 
     /* check if the current point is inside direct polyhedron */
 
+    const vec3 hpos = pos * (1.0/f);
     for (int i = 0; i < n; i++)
-      if (!list[i].inside(pos))
+      if (!list[i].inside(hpos))
         return false;
 
     /* if so, find out if there are reduntant points */
 
-    const HalfSpace h(pos);
+    const HalfSpace h(pos*f);
     int i = -1;
     while (++i < n)
       if (!h.inside(list[i].p))
       {
+#if 0
+        if (nb[i] == 20850 || nb[i] == 42255 || nb[i] == 0)
+        {
+          fprintf(stderr, "i= %d: pi= %g %g %g  | %g\n", nb[i], list[i].p.x, list[i].p.y, list[i].p.z, list[i].p.norm2());
+          fprintf(stderr, "j= %d: pj= %g %g %g  | %g\n", idx, pos.x, pos.y, pos.z, pos.norm2());
+          fprintf(stderr, "%g\n", pos*list[i].p);
+        }
+        //    assert(nb[i] != 20850);
+#endif
         std::swap(list[i  ], list[--n]);
         std::swap(  nb[i--],   nb[  n]);
       }
 
     /* now store the new point */
     assert(n < N);
-    list[n  ] = h;
+    list[n  ] = HalfSpace(pos);
     nb  [n++] = idx;
 
     return true;
--- vorodia/testdirect.cpp
+++ vorodia/testdirect.cpp
@@ -34,14 +34,18 @@ int main (int argc, char * argv [])
   for (int i = 0; i < np; i++)
     ptcl[i] = Particle(vec3(drand48(), drand48(), drand48()), i);
 
-  const int NFACEMAX = 32;
+  const int NFACEMAX = 128;
   DirectPolyhedron<NFACEMAX> d;
   Voronoi::Cell<NFACEMAX> cell;
   Voronoi::Site::Vector list;
   list.reserve(NFACEMAX);
 
   const double t0 = get_wtime();
-  const int cnt = 10;
+  const int cnt = 15;
+  std::vector<int> nblist;
+  std::swap(ptcl[0], ptcl[42255]);
+  ptcl[0].idx = 0;
+  ptcl[42255].idx = 42255;
   for (int icnt = 0; icnt < cnt; icnt++)
   {
     const real f = 0.05;
@@ -49,10 +53,19 @@ int main (int argc, char * argv [])
         0.5 + (1.0 - 2.0*drand48()) * f,
         0.5 + (1.0 - 2.0*drand48()) * f,
         0.5 + (1.0 - 2.0*drand48()) * f);
+
+#if 0
+    if (icnt < 1) continue;
+    if (icnt > 1) break;
+#endif
     d.clear();
     int nsuccess = 0;
     for (int i= 0; i < np; i++)
-      nsuccess += d.push(ptcl[i].pos - pos, i);
+      nsuccess += d.push((ptcl[i].pos - pos)*0.5, i, 1.25);
+#if 0
+    for (int i= 0; i < np; i++)
+      nsuccess += d.push((ptcl[i].pos - pos)*0.5, i);
+#endif
     fprintf(stderr, " --- \n");
     fprintf(stderr, " icnt= %d: nsuccess= %d  nface= %d\n", icnt, nsuccess, d.nface());
 
@@ -60,21 +73,49 @@ int main (int argc, char * argv [])
     list.clear();
     for (int i = 0; i < d.nface(); i++)
       list.push_back(Voronoi::Site(ptcl[d[i]].pos-pos, d[i]));
+#if 0
+    list.push_back(Voronoi::Site(vec3(-2.0*pos.x,0.0,0.0), -1));
+    list.push_back(Voronoi::Site(vec3(2.0*(1.0-pos.x),0.0,0.0), -2));
+    list.push_back(Voronoi::Site(vec3(0.0, -2.0*pos.y,0.0), -3));
+    list.push_back(Voronoi::Site(vec3(0.0, 2.0*(1.0-pos.y),0.0), -4));
+    list.push_back(Voronoi::Site(vec3(0.0, 0.0, -2.0*pos.z), -5));
+    list.push_back(Voronoi::Site(vec3(0.0, 0.0, 2.0*(1.0-pos.z)), -6));
+#endif
     cell.build(list);
     fprintf(stderr, "  nf= %d vol= %g\n", cell.nb(), cell.volume());
 #endif
     
 #if 0
+    nblist.clear();
+    for (int i = 0; i < d.nface(); i++)
+      nblist.push_back(ptcl[d[i]].idx);
+
     d.clear();
+    Particle::Vector ptcl1(ptcl);
     for (int i= 0; i < np; i++)
-      ptcl[i].r2 = (ptcl[i].pos - pos).norm2();
-    std::sort(ptcl.begin(), ptcl.end(), Particle());
+      ptcl1[i].r2 = (ptcl1[i].pos - pos).norm2()*0.25;
+    std::sort(ptcl1.begin(), ptcl1.end(), Particle());
     nsuccess = 0;
 #if 1
     for (int i= 0; i < np; i++)
-      nsuccess += d.push(ptcl[i].pos - pos);
+      nsuccess += d.push((ptcl1[i].pos - pos)*0.5, i);
 #endif
     fprintf(stderr, "  sorted: nsuccess= %d  nface= %d\n", nsuccess, d.nface());
+
+#if 0
+    std::sort(nblist.begin(), nblist.end());
+    for (int i = 0; i < (int)nblist.size(); i++)
+      fprintf(stderr, " %d", nblist[i]);
+    fprintf(stderr, " \n");
+
+    nblist.clear();
+    for (int i = 0; i < d.nface(); i++)
+      nblist.push_back(ptcl1[d[i]].idx);
+    std::sort(nblist.begin(), nblist.end());
+    for (int i = 0; i < d.nface(); i++)
+      fprintf(stderr, " %d", nblist[i]);
+    fprintf(stderr, " \n");
+#endif
 #endif
   }
   const double dt = get_wtime() - t0;
