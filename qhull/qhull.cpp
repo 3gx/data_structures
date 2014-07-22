e0dc7e73199aa991d756c963a7e32531c17a4602
evghenii <egaburov@dds.nl>
2014-07-22 21:58:17 +0200

    +1

--- qhull/qhull.cpp
+++ qhull/qhull.cpp
@@ -1,5 +1,28 @@
 #include "qhull.h"
 
+template<> 
+void 
+QHull::extremeSimplexR<2>::eval(const QHull::pos_t::vector &pos, QHull::Simplex_t &simplex)
+{
+  real_t xMin = +HUGE, xMax = -HUGE;
+  const int np = pos.size();
+  for (int i = 0; i < np; i++)
+  {
+    const auto &p = pos[i];
+    if (p[0] < xMin)
+    {
+      xMin       = p[0];
+      simplex[0] = p;
+    }
+    if (p[0] > xMax)
+    {
+      xMax       = p[0];
+      simplex[1] = p;
+    }
+  }
+}
+
+
 int main(int argc, char*argv[])
 {
   QHull q;
--- qhull/qhull.h
+++ qhull/qhull.h
@@ -139,228 +139,205 @@ class Vector_t
 
 struct QHull
 {
-  enum {NDIM = 3};
-  typedef float real_t;
-  typedef int   id_t;
+  public:
+    enum {NDIM = 3};
+    typedef float real_t;
+    typedef int   id_t;
 
-  typedef Vector_t <real_t,NDIM> vec_t;
+    typedef Vector_t <real_t,NDIM> vec_t;
 
-  struct pos_t
-  {
-    typedef std::vector<pos_t> vector;
-    vec_t pos;
-    id_t  idx;
-    real_t& operator[](const int i)       { return pos[i]; }
-    real_t  operator[](const int i) const { return pos[i]; }
-  };
-  typedef std::array<pos_t,NDIM> vtx_t;
-
-  struct Facet
-  {
-    typedef std::list<Facet> list;
-    typedef list::iterator iterator;
-
-    std::pair<vec_t,real_t> plane;
-    vtx_t vtx;     /* vertecies are stored in right-handed orientation */
-    real_t distance(const vec_t &pos) const
+    struct pos_t
     {
-      return dot(plane.first,pos) + plane.second;
-    }
+      typedef std::vector<pos_t> vector;
+      vec_t pos;
+      id_t  idx;
+      real_t& operator[](const int i)       { return pos[i]; }
+      real_t  operator[](const int i) const { return pos[i]; }
+    };
+    typedef std::array<pos_t,NDIM> vtx_t;
 
-    static std::pair<vec_t,real_t> makePlane(const vtx_t &vtx)
+    struct Facet
     {
-      /* move origin to the plane */
-      vtx_t vtxP;
-      for (int l = 0; l < NDIM-1; l++)
-        vtxP[l].pos = vtx[l].pos - vtx[NDIM-1].pos;
+      typedef std::list<Facet> list;
+      typedef list::iterator iterator;
 
-      /* find a unit vector that is not parallel to the plane */
-      vec_t unitVec(0.0);
-      unitVec[0] = 1.0;
-
-      int el = 0;
-      vec_t planeVec = vtxP[el++].pos;
-      while (dot(planeVec,unitVec) == 0)
+      std::pair<vec_t,real_t> plane;
+      vtx_t vtx;     /* vertecies are stored in right-handed orientation */
+      real_t distance(const vec_t &pos) const
       {
-        planeVec = vtxP[el++].pos;
-        assert(el < NDIM-1);
+        return dot(plane.first,pos) + plane.second;
       }
 
-      /* compute plane equation */
-      vec_t n = unitVec - unitVec*(planeVec * (1.0/sqrt(norm2(planeVec))));
-      n *= 1.0/sqrt(norm2(n));
-      real_t p = dot(n,planeVec);
+      static std::pair<vec_t,real_t> makePlane(const vtx_t &vtx)
+      {
+        /* move origin to the plane */
+        vtx_t vtxP;
+        for (int l = 0; l < NDIM-1; l++)
+          vtxP[l].pos = vtx[l].pos - vtx[NDIM-1].pos;
 
-      return std::make_pair(n,p);
-    }
+        /* find a unit vector that is not parallel to the plane */
+        vec_t unitVec(0.0);
+        unitVec[0] = 1.0;
 
-    Facet makeFace(const pos_t &p, const int facetIdx) const
+        int el = 0;
+        vec_t planeVec = vtxP[el++].pos;
+        while (dot(planeVec,unitVec) == 0)
+        {
+          planeVec = vtxP[el++].pos;
+          assert(el < NDIM-1);
+        }
+
+        /* compute plane equation */
+        vec_t n = unitVec - unitVec*(planeVec * (1.0/sqrt(norm2(planeVec))));
+        n *= 1.0/sqrt(norm2(n));
+        real_t p = dot(n,planeVec);
+
+        return std::make_pair(n,p);
+      }
+
+      Facet makeFace(const pos_t &p, const int facetIdx) const
+      {
+        assert(facetIdx >= 0 && facetIdx < NDIM);
+
+        Facet f;
+        f.vtx[facetIdx] = p;
+        for (int i = 0; i < NDIM; i++)
+        {
+          if (facetIdx != i)
+            f.vtx[i] = vtx[i];
+        }
+
+        f.plane = makePlane(f.vtx);
+
+        return f;
+      }
+    };
+    Facet::list facetList;
+
+    struct FacetMD  /* facet metadata */
     {
-      assert(facetIdx >= 0 && facetIdx < NDIM);
+      typedef std::stack<FacetMD> stack;
+      Facet::iterator it;
+      pos_t *pBuf;
+      int pbeg, pend;
+      FacetMD(Facet::iterator _it, pos_t *_pBuf, int _pbeg, int _pend) :
+        it(_it), pBuf(_pBuf), pbeg(_pbeg), pend(_pend) {}
+    };
+    FacetMD::stack facetStack;
 
-      Facet f;
-      f.vtx[facetIdx] = p;
+    bool partition(const FacetMD &fmd, pos_t *pBuf)
+    {
+      const int np = fmd.pend - fmd.pbeg;
+      /* no particles left to partition */
+      if (np == 0)
+        return false;
+
+      /* find max distances */
+      real_t distMax = 0;
+      pos_t  pMax;
+      for (int i = fmd.pbeg; i < fmd.pend; i++)
+      {
+        const pos_t    &p = fmd.pBuf[i];
+        const real_t dist = fmd.it->distance(p.pos);
+        if (dist > distMax)
+        {
+          distMax = dist;
+          pMax    = p;
+        }
+      }
+      assert(distMax > 0);
+
+      Facet facets[NDIM];
       for (int i = 0; i < NDIM; i++)
+        facets[i] = fmd.it->makeFace(pMax, i);
+
+      /* count particles belonging to each of the new face */
+
+      int count[NDIM] = {0};
+      std::vector<int> whichFacet(np);
+
+      for (int i = fmd.pbeg; i < fmd.pend; i++)
       {
-        if (facetIdx != i)
-          f.vtx[i] = vtx[i];
+        const auto &p = fmd.pBuf[i].pos;
+        bool used = false;
+        for (int l = 0; l < NDIM; l++)
+          if (facets[l].distance(p) >= 0)
+          {
+            assert(!used);
+            count[l]++;
+            whichFacet[i-fmd.pbeg] = l;
+            used = true;
+          }
       }
 
-      f.plane = makePlane(f.vtx);
-
-      return f;
-    }
-  };
-  Facet::list facetList;
-
-  struct FacetMD  /* facet metadata */
-  {
-    typedef std::stack<FacetMD> stack;
-    Facet::iterator it;
-    pos_t *pBuf;
-    int pbeg, pend;
-    FacetMD(Facet::iterator _it, pos_t *_pBuf, int _pbeg, int _pend) :
-      it(_it), pBuf(_pBuf), pbeg(_pbeg), pend(_pend) {}
-  };
-  FacetMD::stack facetStack;
-
-  bool partition(const FacetMD &fmd, pos_t *pBuf)
-  {
-    const int np = fmd.pend - fmd.pbeg;
-    /* no particles left to partition */
-    if (np == 0)
-      return false;
-
-    /* find max distances */
-    real_t distMax = 0;
-    pos_t  pMax;
-    for (int i = fmd.pbeg; i < fmd.pend; i++)
-    {
-      const pos_t    &p = fmd.pBuf[i];
-      const real_t dist = fmd.it->distance(p.pos);
-      if (dist > distMax)
+      /* compute offset */
+      int pbeg[NDIM] = {0};
+      int pend[NDIM] = {0};
+      for (int l = 1; l < NDIM; l++)
       {
-        distMax = dist;
-        pMax    = p;
+        pbeg[l] = pbeg[l-1] + count[l-1] + fmd.pbeg;
+        pend[l] = pbeg[l  ];
       }
-    }
-    assert(distMax > 0);
+      assert(pbeg[NDIM-1] + count[NDIM-1] == fmd.pend - fmd.pbeg);
 
-    Facet facets[NDIM];
-    for (int i = 0; i < NDIM; i++)
-      facets[i] = fmd.it->makeFace(pMax, i);
+      /* sort */
+      for (int i = fmd.pbeg; i < fmd.pend; i++)
+      {
+        const int l = whichFacet[i-fmd.pbeg];
+        pBuf[pend[l]++] = fmd.pBuf[i];
+      }
 
-    /* count particles belonging to each of the new face */
+      /* remove old face */
+      facetList.erase(fmd.it);
 
-    int count[NDIM] = {0};
-    std::vector<int> whichFacet(np);
-
-    for (int i = fmd.pbeg; i < fmd.pend; i++)
-    {
-      const auto &p = fmd.pBuf[i].pos;
-      bool used = false;
+      /* push new faces to the stack */
       for (int l = 0; l < NDIM; l++)
-        if (facets[l].distance(p) >= 0)
-        {
-          assert(!used);
-          count[l]++;
-          whichFacet[i-fmd.pbeg] = l;
-          used = true;
-        }
-    }
-
-    /* compute offset */
-    int pbeg[NDIM] = {0};
-    int pend[NDIM] = {0};
-    for (int l = 1; l < NDIM; l++)
-    {
-      pbeg[l] = pbeg[l-1] + count[l-1] + fmd.pbeg;
-      pend[l] = pbeg[l  ];
-    }
-    assert(pbeg[NDIM-1] + count[NDIM-1] == fmd.pend - fmd.pbeg);
-
-    /* sort */
-    for (int i = fmd.pbeg; i < fmd.pend; i++)
-    {
-      const int l = whichFacet[i-fmd.pbeg];
-      pBuf[pend[l]++] = fmd.pBuf[i];
-    }
-
-    /* remove old face */
-    facetList.erase(fmd.it);
-
-    /* push new faces to the stack */
-    for (int l = 0; l < NDIM; l++)
-    {
-      auto it = fmd.it;
-      facetList.insert(it, facets[l]);
-      it--;
-      facetStack.push(FacetMD(it, pBuf, pbeg[l], pend[l]));
-    }
-
-    return true;
-  }
-
-  void extremeSimplex(const pos_t::vector &pos)
-  {
-    facetList.clear();
-    std::array<pos_t,NDIM+1> vtxList;
-
-    real_t xMin = +HUGE, xMax = -HUGE;
-    const int np = pos.size();
-    for (int i = 0; i < np; i++)
-    {
-      const auto &p = pos[i];
-      if (p[0] < xMin)
       {
-        xMin       = p[0];
-        vtxList[0] = p;
-      }
-      if (p[0] > xMax)
-      {
-        xMax       = p[0];
-        vtxList[1] = p;
-      }
-    }
-    assert(vtxList[0].idx != vtxList[1].idx);
-
-    for (int l = 2; l < NDIM+1; l++)
-    {
-      real_t dMax = -HUGE;
-      for (int i = 0; i < np; i++)
-      {
-//        bool use = true;
-        real_t d = 0, n = 0;
-        for (int ll = 0; ll < l; ll++)
-        {
-//          d += 
-
-        }
+        auto it = fmd.it;
+        facetList.insert(it, facets[l]);
+        it--;
+        facetStack.push(FacetMD(it, pBuf, pbeg[l], pend[l]));
       }
 
-
+      return true;
     }
 
-  }
+  private:
+    typedef std::array<pos_t,NDIM+1> Simplex_t;
 
-  void computeConvexHull(const pos_t::vector &pos)
-  {
-    pos_t::vector pos1(pos), pos2(pos.size());
-
-    extremeSimplex(pos);
-
-    while (!facetStack.empty())
-      facetStack.pop();
-
-    pos_t *pBuf1 = &pos1[0];
-    pos_t *pBuf2 = &pos2[0];
-    while (!facetStack.empty())
+    template<int N> struct extremeSimplexR
     {
-      const auto fmd = facetStack.top();
-      facetStack.pop();
-      partition(fmd, fmd.pBuf == pBuf1 ? pBuf2 : pBuf1);
+      static void eval(const pos_t::vector &pos, Simplex_t &simplex)
+      {
+        extremeSimplexR<N-1>::eval(pos, simplex);
+      }
+    };
+    
+    void extremeSimplex(const pos_t::vector &pos)
+    {
+      Simplex_t simplex;
+      extremeSimplexR<NDIM+1>::eval(pos,simplex);
     }
-  }
+
+
+    void computeConvexHull(const pos_t::vector &pos)
+    {
+      pos_t::vector pos1(pos), pos2(pos.size());
+
+      extremeSimplex(pos);
+
+      while (!facetStack.empty())
+        facetStack.pop();
+
+      pos_t *pBuf1 = &pos1[0];
+      pos_t *pBuf2 = &pos2[0];
+      while (!facetStack.empty())
+      {
+        const auto fmd = facetStack.top();
+        facetStack.pop();
+        partition(fmd, fmd.pBuf == pBuf1 ? pBuf2 : pBuf1);
+      }
+    }
 
 
 };
