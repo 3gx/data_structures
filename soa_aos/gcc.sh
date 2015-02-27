2bc3c8b1cdb3e0adb236432619349395a81bb7f8
Evghenii Gaburov <egaburov@dds.nl>
2015-02-27 09:40:31 +0100

    added compile strings

---
+++ soa_aos/gcc.sh
@@ -0,0 +1,1 @@
+ g++ aossoa.cpp -std=c++11 -O3 -mavx2 -ftree-vectorize -ffast-math  -ftree-vectorizer-verbose=1 -fstrict-aliasing
---
+++ soa_aos/icc.sh
@@ -0,0 +1,1 @@
+icc aossoa.cpp -std=c++11 -O3 -S -xcore-avx512 -qopt-report -qopt-report-phase=vec   -qopt-report-file=stdout -fno-alias
